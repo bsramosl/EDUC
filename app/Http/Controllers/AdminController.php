62314909226dd24dbe877f6a7baa82e933b1b9b6
Lopez <steeven10f@gmail.com>
2021-12-24 12:44:28 -0500

    24

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\User;
 use App\Models\Curso;
 use App\Models\Evento;
+use App\Models\PagoEvento;
 
 class AdminController extends Controller
 {
@@ -18,6 +19,7 @@ class AdminController extends Controller
 
     public function listar(){
         $data['evento'] = Evento::paginate(10);
+        $data['pagoevento'] = PagoEvento::all();
         return view('dashboard.visualizarevento',$data);
     }
   
--- resources/views/dashboard/visualizarevento.blade.php
+++ resources/views/dashboard/visualizarevento.blade.php
@@ -8,81 +8,44 @@
 <b>{{auth()->user()->name }}</b>
 
 @endsection
- 
 
 @section('content') 
 <div class="card">
     <div class="card-header">
       <h3 class="card-title"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Eventos</font></font></h3>
-
-      
     </div>
     <div class="card-body p-0">
       <table class="table table-striped projects">
           <thead>
               <tr>
-                  <th style="width: 1%"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                      #
-                  </font></font></th>
-                  <th style="width: 20%"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                      Nombre del proyecto
-                  </font></font></th>
-                  <th style="width: 30%"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                      Miembros del equipo
-                  </font></font></th>
-                  <th><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                      Progreso del proyecto
-                  </font></font></th>
-                  <th style="width: 8%" class="text-center"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                      Estado
-                  </font></font></th>
-                  <th style="width: 20%">
-                  </th>
+                  <th style="width: 1%">#</th>
+                  <th style="width: 20%">Nombre del Evento</th>
+                  <th style="width: 30%">Descripcion</th>
+                  <th style="width: 10%">Creado</th> 
+                  <th style="width: 10%" class="text-center"> stado</th>
+                  <th style="width: 10%">Opciones </th>
               </tr>
           </thead>
           <tbody>
-              <tr>
-                  @foreach ($evento as $even)
-
-                  <td><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                    #
-                </font></font></td>
+            @foreach ($evento as $even)
+              <tr> 
+                  <td>#</td>
                 <td>
-                    <a><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                        {{$even->nombre}}
-                     </font></font></a>
-                    <br>
-                    <small><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                        Creado {{$even->fecha}}
-                    </font></font></small>
+                    <a> {{$even->nombre}}   </a> 
                 </td>
                 <td>
-                    <ul class="list-inline">
-                        <li class="list-inline-item">
-                            <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar.png">
-                        </li>
-                        <li class="list-inline-item">
-                            <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar2.png">
-                        </li>
-                        <li class="list-inline-item">
-                            <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar3.png">
-                        </li>
-                        <li class="list-inline-item">
-                            <img alt="Avatar" class="table-avatar" src="../../dist/img/avatar4.png">
-                        </li>
-                    </ul>
-                </td>
-                <td class="project_progress">
-                    <div class="progress progress-sm">
-                        <div class="progress-bar bg-green" role="progressbar" aria-valuenow="57" aria-valuemin="0" aria-valuemax="100" style="width: 57%">
-                        </div>
-                    </div>
-                    <small><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
-                        57% completo
-                    </font></font></small>
-                </td>
+                    {{$even->descripcion}}
+                </td> 
+                <td>
+                    {{$even->fecha}}
+                </td> 
                 <td class="project-state">
-                    <span class="badge badge-success"><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">Éxito</font></font></span>
+                    @if (date("Y-m-d") > $even->fecha) 
+                    <span class="badge badge-success"> Terminado </span>
+                    @else
+                    <span class="badge badge-success"> Activo </span>
+                    @endif
+                    
                 </td>
                 <td class="project-actions text-right">
                     <a class="btn btn-primary btn-sm" href="#">
@@ -90,11 +53,9 @@
                         </i><font style="vertical-align: inherit;"><font style="vertical-align: inherit;">
                         Vista
                     </font></font></a> 
-                </td>
-                      
-                  @endforeach
-                  
-              </tr> 
+                </td> 
+              </tr>
+              @endforeach 
           </tbody>
       </table>
     </div>
